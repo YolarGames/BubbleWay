36a71ea1f27b5af553302635cd54c0462b467576
Nikita Ivanin <nikitaivanin93@gmail.com>
2025-01-26 11:05:17 +0100

    add: more functionality to mainMenu buttons

---
+++ Assets/Code/UI/ExitGameButton.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI
+{
+	public class ExitGameButton : MonoBehaviour
+	{
+		private Button _button;
+
+		private void Awake() =>
+			Debug.Assert(TryGetComponent(out _button), "Button component not found");
+
+		private void OnEnable() =>
+			_button.onClick.AddListener(ExitGame);
+
+		private void OnDisable() =>
+			_button.onClick.RemoveListener(ExitGame);
+
+		private static void ExitGame() =>
+			Application.Quit();
+	}
+}
---
+++ Assets/Code/UI/OnClickAudioPlayer.cs
@@ -0,0 +1,14 @@
+﻿using Audio;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace UI
+{
+	public class OnClickAudioPlayer : MonoBehaviour, IPointerUpHandler
+	{
+		[SerializeField] private RandomAudioProviderSo _audioProvider;
+
+		public void OnPointerUp(PointerEventData eventData) =>
+			AudioSource.PlayClipAtPoint(_audioProvider.GetRandom(), transform.position);
+	}
+}
