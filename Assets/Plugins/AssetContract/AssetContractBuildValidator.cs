2e335ec12ad8d8d4e2fb5b23fec1df227537d28e
Nikita Ivanin <nikitaivanin93@gmail.com>
2025-05-19 22:51:15 +0200

    import: AssetContract package - remove: abilities UI form scene

---
+++ Assets/Plugins/AssetContract/AssetContractBuildValidator.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+using UnityEditor;
+using UnityEditor.Build;
+using UnityEditor.Build.Reporting;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+namespace AssetContract
+{
+	public class AssetContractBuildValidator : IPreprocessBuildWithReport
+	{
+		public int callbackOrder => 0;
+
+		public void OnPreprocessBuild(BuildReport _)
+		{
+			var violations = new List<string>();
+
+			MonoBehaviour[] allBehaviours =
+				Object.FindObjectsByType<MonoBehaviour>(FindObjectsInactive.Include, FindObjectsSortMode.None);
+
+			foreach (MonoBehaviour behaviour in allBehaviours)
+				ValidateObjectContracts(behaviour, violations);
+
+			ScriptableObject[] allScriptables = LoadAllAssets<ScriptableObject>();
+			foreach (ScriptableObject scriptable in allScriptables)
+				ValidateObjectContracts(scriptable, violations);
+
+			if (violations.Count > 0)
+			{
+				string msg = "Asset Contract Violations:\n" + string.Join("\n", violations);
+				throw new BuildFailedException(msg);
+			}
+		}
+
+		private static void ValidateObjectContracts(Object target, List<string> violations)
+		{
+			FieldInfo[] fields = target
+				.GetType()
+				.GetFields(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
+
+			string objectProjectPath = GetTargetObjectProjectPath(target);
+
+			foreach (FieldInfo field in fields)
+			{
+				object[] attrs = field.GetCustomAttributes(typeof(AssetContractAttributeBase), true);
+				if (attrs.Length == 0)
+					continue;
+
+				var asset = field.GetValue(target) as Object;
+				if (!asset)
+					violations.Add($"{objectProjectPath}.{field.Name}: Maybe you forgot to assign this field?");
+
+				var contract = (AssetContractAttributeBase)attrs[0];
+				if (!contract.IsSupportedFieldType(field.FieldType, out string error))
+					violations.Add($"{objectProjectPath}.{field.Name}: {error}");
+
+				if (!contract.IsValid(asset, out error))
+					violations.Add($"{objectProjectPath}.{field.Name}: {error}");
+			}
+		}
+
+		private static string GetTargetObjectProjectPath(Object obj)
+		{
+			if (obj is not MonoBehaviour monoBehaviour)
+				return obj.name;
+
+			if (PrefabUtility.IsPartOfPrefabAsset(monoBehaviour))
+				return monoBehaviour.gameObject.name;
+
+			if (monoBehaviour.gameObject.scene.IsValid())
+				return $"{monoBehaviour.gameObject.scene.name}.{monoBehaviour.gameObject.name}";
+
+			return obj.name;
+		}
+
+		private static T[] LoadAllAssets<T>() where T : Object
+		{
+			string[] guids = AssetDatabase.FindAssets($"t:{typeof(T).Name}");
+			List<T> assets = new();
+
+			foreach (string guid in guids)
+			{
+				string path = AssetDatabase.GUIDToAssetPath(guid);
+				var asset = AssetDatabase.LoadAssetAtPath<T>(path);
+				if (asset)
+					assets.Add(asset);
+			}
+
+			return assets.ToArray();
+		}
+	}
+}
---
+++ Assets/Plugins/AssetContract/TextureContractAttribute.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+namespace AssetContract
+{
+	public sealed class TextureContractAttribute : AssetContractAttributeBase
+	{
+		private int MaxFileSizeKb { get; }
+		private string Extension { get; }
+		private Vector2Int Resolution { get; }
+
+		public TextureContractAttribute(
+			int width, int height,
+			int maxFileSizeKb = 0,
+			string extension = null)
+		{
+			Resolution = new Vector2Int(width, height);
+			MaxFileSizeKb = maxFileSizeKb;
+			Extension = extension;
+		}
+
+		public override bool IsValid(Object asset, out string error)
+		{
+			error = null;
+
+			if (!asset)
+				return false;
+
+			Texture2D texture = asset switch
+			{
+				Sprite sprite => sprite.texture,
+				Texture2D texture2D => texture2D,
+				_ => null,
+			};
+
+			if (!texture)
+				return false;
+
+			if (!IsCorrectResolution(texture))
+				error += $"Resolution must be {Resolution.x}x{Resolution.y}. ";
+
+			if (!IsCorrectFileSize(texture))
+				error += $"File size must be <= {MaxFileSizeKb} KB. ";
+
+			if (!IsRightFormat(texture))
+				error += $"Texture must be in {Extension} format.";
+
+			return error == null;
+		}
+
+		public override bool IsSupportedFieldType(Type fieldType, out string error)
+		{
+			error = $"Asset type {fieldType.Name} is not supported target of attribute {GetType().Name}.";
+			return fieldType == typeof(Texture2D)
+			       || fieldType == typeof(Sprite);
+		}
+
+		private bool IsCorrectResolution(Texture2D texture) =>
+			Resolution is { x: <= 0, y: <= 0 }
+			|| (texture.width == Resolution.x
+			    && texture.height == Resolution.y);
+
+		private bool IsCorrectFileSize(Texture2D texture) =>
+			MaxFileSizeKb == 0 || texture.GetFileSizeKb() <= MaxFileSizeKb;
+
+		private bool IsRightFormat(Texture2D texture) =>
+			Extension == null || texture.IsExtensionMatches(Extension);
+	}
+}
